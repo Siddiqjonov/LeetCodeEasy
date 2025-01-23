3d7394d157b4125ed8f28ec4776017d17173ac58
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-01-23 09:06:14 +0500

    1.Two Sum

---
+++ 001_1.TwoSum/TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
