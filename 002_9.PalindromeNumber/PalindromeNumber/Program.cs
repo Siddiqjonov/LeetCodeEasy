ec3b8ec771939262e1a2188713a0d42eb927a89d
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-01-23 09:08:38 +0500

    9.Palindrome Number

---
+++ 002_9.PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string xIntoStr = Convert.ToString(x);
+
+            string reversed = "";
+
+            for (int i = xIntoStr.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + xIntoStr[i];
+            }
+
+            if (xIntoStr == reversed)
+                return true;
+            else
+                return false;
+        }
+    }
+}
